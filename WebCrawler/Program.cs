59ac8aa6d42d77549731877655987ad777e8e529
MaverickCheang <78773070+stanleychan0507@users.noreply.github.com>
2024-04-22 09:46:32 +0800

    first commit

---
+++ WebCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace WebCrawler;
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        var temp = new WebCrawler();
+        await temp.StartWebCrawlAsync();
+    }
+}
---
+++ WebCrawler/WebCrawler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.JavaScript;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Headers;
+
+
+namespace WebCrawler
+{
+    class WebCrawler
+    {
+
+        public async Task StartWebCrawlAsync()
+        {
+            Console.OutputEncoding  = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+
+            var            url          = "https://www.srleng.edu.mo/";
+            var            httpClient   = new HttpClient();
+            var            html         = await httpClient.GetStringAsync(url);
+
+            var response = await httpClient.GetAsync(url);
+            var header   = response.Content.Headers;
+            Console.WriteLine(header);
+
+            var            htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var divs =
+                    htmlDocument.DocumentNode.Descendants("div")
+                                .Where(node => node.GetAttributeValue("class", "").Equals("announcementscroll")).ToList();
+            foreach (var div in divs)
+            {
+                var result = div.Descendants("ul").FirstOrDefault().InnerText;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
